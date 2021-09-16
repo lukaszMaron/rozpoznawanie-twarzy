d0a5852bf3a16ae13fa33997c6f06797e325fbc0
Łukasz Maroń <43815281+lukaszMaron@users.noreply.github.com>
2021-09-17 01:48:14 +0200

    works fine

--- live_recognition.py
+++ live_recognition.py
@@ -5,54 +5,10 @@ import os
 import os.path
 import pickle
 from PIL import Image, ImageDraw
-import face_recognition
-from face_recognition import image_files_in_folder
+import recognition
+from recognition import image_files_in_folder
 import numpy as np
 
-
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'JPG'}
-
-
-def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
-
-    X = []
-    y = []
-
-    # Loop through each person in the training set
-    for class_dir in os.listdir(train_dir):
-        if not os.path.isdir(os.path.join(train_dir, class_dir)):
-            continue
-
-        # Loop through each training image for the current person
-        for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
-            image = face_recognition.load_image_file(img_path)
-            face_bounding_boxes = face_recognition.face_locations(image)
-
-            if len(face_bounding_boxes) != 1:
-                # If there are no people (or too many people) in a training image, skip the image.
-                if verbose:
-                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
-            else:
-                # Add face encoding for current image to the training set
-                X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
-                y.append(class_dir)
-
-    # Determine how many neighbors to use for weighting in the KNN classifier
-    if n_neighbors is None:
-        n_neighbors = int(round(math.sqrt(len(X))))
-        if verbose:
-            print("Chose n_neighbors automatically:", n_neighbors)
-
-    # Create and train the KNN classifier
-    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
-    knn_clf.fit(X, y)
-
-    # Save the trained KNN classifier
-    if model_save_path is not None:
-        with open(model_save_path, 'wb') as f:
-            pickle.dump(knn_clf, f)
-
-    return knn_clf
 
 
 def predict(X_frame, knn_clf=None, model_path=None, distance_threshold=0.5):
@@ -65,14 +21,14 @@ def predict(X_frame, knn_clf=None, model_path=None, distance_threshold=0.5):
         with open(model_path, 'rb') as f:
             knn_clf = pickle.load(f)
 
-    X_face_locations = face_recognition.face_locations(X_frame)
+    X_face_locations = recognition.face_locations(X_frame)
 
     # If no faces are found in the image, return an empty result.
     if len(X_face_locations) == 0:
         return []
 
     # Find encodings for faces in the test image
-    faces_encodings = face_recognition.face_encodings(X_frame, known_face_locations=X_face_locations)
+    faces_encodings = recognition.face_encodings(X_frame, known_face_locations=X_face_locations)
 
     # Use the KNN model to find the best matches for the test face
     closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
@@ -114,9 +70,9 @@ def show_prediction_labels_on_image(frame, predictions):
 
 
 if __name__ == "__main__":
-    print("Training KNN classifier...")
-    classifier = train("knn_examples/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
-    print("Training complete!")
+    #print("Training KNN classifier...")
+    #classifier = train("knn_examples/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
+    #print("Training complete!")
     # process one frame in every 30 frames for speed
     process_this_frame = 29
     print('Setting cameras up...')
